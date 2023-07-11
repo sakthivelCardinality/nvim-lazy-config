ba762127ed04762a7f026b8580344fe2a1008b8b
sakthivelCardinality <sakthivel.a@cardinality.ai>
2023-07-11 12:19:22 +0530

    :tada: :sparkles: plugins updated and themes

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -50,10 +50,50 @@ return {
     end,
   },
   {
+    "justinsgithub/oh-my-monokai.nvim",
+    config = function()
+      require("oh-my-monokai").setup({
+        transparent_background = true,
+        terminal_colors = true,
+        devicons = true, -- highlight the icons of `nvim-web-devicons`
+        palette = "default", -- or create your own ^^ e.g. justinsgithub
+        inc_search = "background", -- underline | background
+        background_clear = {
+          "float_win",
+          "toggleterm",
+          "telescope",
+          "which-key",
+          "renamer",
+          "neo-tree",
+        },
+        plugins = {
+          bufferline = {
+            underline_selected = false,
+            underline_visible = false,
+          },
+          indent_blankline = {
+            context_start_underline = false,
+          },
+        },
+        override = function()
+          return {
+            LineNr = {
+              fg = "#8400ff",
+            },
+            CursorLineNr = {
+              fg = "#fff000",
+            },
+          }
+        end,
+      })
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "tokyonight",
-      colorscheme = "ayu",
+      -- colorscheme = "ayu",
+      colorscheme = "oh-my-monokai",
     },
   },
 }
