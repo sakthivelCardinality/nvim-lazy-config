f8f904ad2f0882151f4fd160c6685bdac1ff2860
sakthivelCardinality <sakthivel.a@cardinality.ai>
2023-07-12 13:23:41 +0530

    change the keymap setting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,26 +2,40 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>fs", "<cmd> Telescope lsp_document_symbols <CR>", { desc = "Symbols" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move down half page center page" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Move up half page center page" })
-vim.keymap.set("n", "n", "nzzzv", { desc = "Search word with half page" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Search word with half page" })
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
 
--- vim.keymap.set("n", "<leader>/", "<cmd> Telescope buffers <CR>", { desc = "Telescope Buffer" })
+map("n", "<leader>fs", "<cmd> Telescope lsp_document_symbols <CR>", { desc = "Symbols" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Move down half page center page" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Move up half page center page" })
+map("n", "n", "nzzzv", { desc = "Search word with half page" })
+map("n", "N", "Nzzzv", { desc = "Search word with half page" })
 
-vim.keymap.set("n", "<C-c>", "<cmd> %y+ <CR>", { desc = "Copy whole file" })
+map("n", "<C-c>", "<cmd> %y+ <CR>", { desc = "Copy whole file" })
 
-vim.keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search()<CR>', {
+map("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search()<CR>', {
   desc = "Search on current file",
 })
+map("i", "<C-h>", "<Left>", { desc = "Move Left" })
+map("i", "<C-l>", "<Right>", { desc = "Move Right" })
 
--- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected content down" })
--- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected content up" })
+-- map("n", "<leader>/", "<cmd> Telescope buffers <CR>", { desc = "Telescope Buffer" })
 
--- vim.keymap.set("i", "<C-b>", "<ESC>^i", { desc = "Beginning of line" })
--- vim.keymap.set("i", "<C-e>", "<End>", { desc = "End of line" })
--- vim.keymap.set("i", "<C-h>", "<Left>", { desc = "Move Left" })
--- vim.keymap.set("i", "<C-l>", "<Right>", { desc = "Move Right" })
--- vim.keymap.set("i", "<C-j>", "<Down>", { desc = "Move Down" })
--- vim.keymap.set("i", "<C-k>", "<Up>", { desc = "Move Up" })
+-- map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected content down" })
+-- map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected content up" })
+
+-- map("i", "<C-b>", "<ESC>^i", { desc = "Beginning of line" })
+-- map("i", "<C-e>", "<End>", { desc = "End of line" })
+-- map("i", "<C-j>", "<Down>", { desc = "Move Down" })
+-- map("i", "<C-k>", "<Up>", { desc = "Move Up" })
